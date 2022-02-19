4cb17541fb19397b410f52e15ee43c61d2fb6232
natusm1891 <natuss.m@gmail.com>
2022-02-19 18:11:31 -0300

    funcionando  +-
    funcionando  +-

--- files/script.js
+++ files/script.js
@@ -1,7 +1,12 @@
-let start = 0;
-let end = 0;
+let startC = 0;
+let endC = 0;
 let total
-//let crono = new Date();
+let cronoMs = 0;
+let cronoS = 0;
+let cronoM = 0;
+let cronoH = 0;
+let resto = 0;
+let parando = false;
 
 const ids = ["hora", "min", "seg"];
 const idCrono = ["cronoH", "cronoM", "cronoS", "cronoMs"];
@@ -18,29 +23,28 @@ function updateClock(){
 }
 
 
-function updateCrono(hora, minutos, segundos, milisegundos){
-    let valores = [hora, minutos, segundos, milisegundos] //CONTINUAR DAQUI
-    end = Date.now();
-    total = (end - start);
-    console.log(total);
-    let cronoMs = 0;
-    let cronoS = 0;
-    let cronoM = 0;
-    let cronoH = 0;
-    if(total > (60*60*1000)){
-        cronoH = (total/(60*60*1000))
-        console.log(cronoH) 
-    }
-    if(total > (60*1000)){
-        cronoM = (total/(60*1000))
-        console.log(cronoM) 
-    }
-    if(total > (1000)){
-        cronoS = (total/(1000))
-        console.log(cronoS) 
-    }
+function updateCrono(){
+    endC = Date.now();
+    total = (endC - startC);
+
+
+    cronoH = Math.trunc(total/(60*60*1000))
+    total = total - cronoH*60
+
     
-    for(let i=0; i < idCrono.length-1; i++)
+   
+    cronoM = Math.trunc(total/(60*1000))
+    total = total - cronoM*60
+
+    
+
+    cronoS = Math.trunc(total/(1000))
+    total = total - cronoS*1000
+ 
+    cronoMs = total;
+
+    let valores = [cronoH, cronoM, cronoS, cronoMs]
+    for(let i=0; i < idCrono.length; i++)
         document.getElementById(idCrono[i]).innerHTML = valores[i];
 }
 
@@ -50,37 +54,11 @@ function relogio(){
 }
 
 function iniciar(){
-    start = Date.now();
-    setInterval("updateCrono()", 1)
+    startC = Date.now();
+    myInterval = setInterval("updateCrono()", 1)
 }
 
 
 function parar(){
-    console.log("entrou PARAR")
-    end = Date.now();
-    total = (end - start);
-    console.log(total);
-    let cronoMs = 0;
-    let cronoS = 0;
-    let cronoM = 0;
-    let cronoH = 0;
-    if(total > (60*60*1000)){
-        cronoH = (total/(60*60*1000))
-        console.log(cronoH) 
-    }
-    if(total > (60*1000)){
-        cronoM = (total/(60*1000))
-        console.log(cronoM) 
-    }
-    if(total > (1000)){
-        cronoS = (total/(1000))
-        console.log(cronoS) 
-    }
-    
-    cronoMs = total
-
-
-
-
-    //ocument.getElementById("display").innerHTML = total.getMilliseconds().toString()
+    clearInterval(myInterval);
 }
